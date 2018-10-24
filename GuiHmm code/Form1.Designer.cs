de5451670fe3f85728b75fdd6537b740a8cc6924
Anna Vedenin <44424775+annavedenin@users.noreply.github.com>
2018-10-24 19:56:16 +0300

    Add files via upload

---
+++ GuiHmm code/Form1.Designer.cs
@@ -0,0 +1,421 @@
+﻿namespace GuiHMM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.welcomeLable1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // welcomeLable1
+            // 
+            this.welcomeLable1.AutoSize = true;
+            this.welcomeLable1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.welcomeLable1.Font = new System.Drawing.Font("Franklin Gothic Book", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeLable1.Location = new System.Drawing.Point(55, 24);
+            this.welcomeLable1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.welcomeLable1.Name = "welcomeLable1";
+            this.welcomeLable1.Size = new System.Drawing.Size(298, 60);
+            this.welcomeLable1.TabIndex = 0;
+            this.welcomeLable1.Text = "Welcome To HMM\r\nCrime Prediction Program\r\n";
+            this.welcomeLable1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, 138);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Upload File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(205, 138);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Continue Without Upload";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 204);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "*The data set until xx/xx/xx";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Location = new System.Drawing.Point(7, 8);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(399, 287);
+            this.panel2.TabIndex = 6;
+            this.panel2.Visible = false;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(9, 197);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(116, 44);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Show State History\r\n";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(146, 199);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 42);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Back";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(9, 2);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(376, 187);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.comboBox3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Location = new System.Drawing.Point(7, 8);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(399, 287);
+            this.panel1.TabIndex = 4;
+            this.panel1.Visible = false;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.comboBox3.Location = new System.Drawing.Point(293, 125);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(92, 21);
+            this.comboBox3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 128);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Select Crime Resolution\r\n";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(245, 175);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 36);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Get Prediction";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+                "0110,HOMICIDE,FIRST DEGREE MURDER",
+                "0142,HOMICIDE,RECKLESS HOMICIDE",
+                "0261,SEXUAL ASSAULT,HANDGUN",
+                "0263,SEXUAL ASSAULT,KNIFE/CUT INSTR",
+                "0264,SEXUAL ASSAULT,OTHER WEAPON",
+                "0265,SEXUAL ASSAULT,OTHER",
+                "0266,SEXUAL ASSAULT,PREDATORY",
+                "0271,SEXUAL ASSAULT,HANDGUN",
+                "0273,SEXUAL ASSAULT,KNIFE/CUT",
+                "0275,SEXUAL ASSAULT,OTHER",
+                "0281,SEXUAL ASSAULT,AGGRAVATED",
+                "0291,SEXUAL ASSAULT,NON-AGGRAVATED",
+                "0320,ROBBERY,STRONGARM - NO WEAPON",
+                "0330,ROBBERY,AGGRAVATED",
+                "0430,BATTERY,AGG:OTHER WEAPON",
+                "0454,BATTERY,AGG PO NO/MIN INJURY",
+                "0460,BATTERY,SIMPLE",
+                "0486,BATTERY,DOMESTIC SIMPLE",
+                "0496,BATTERY,DOMESTIC: KNIFE",
+                "0497,BATTERY,DOMESTIC WEAPON",
+                "0498,BATTERY,DOMESTIC:SERIOUS INJURY",
+                "0553,ASSAULT,AGG PO: OTHER DANG WEAP",
+                "0560,ASSAULT,SIMPLE",
+                "0610,BURGLARY,FORCIBLE ENTRY",
+                "0630,BURGLARY,ATTEMPT FORCIBLE ENTRY",
+                "0810,THEFT,OVER $500",
+                "0820,THEFT,$500 AND UNDER",
+                "0860,THEFT,RETAIL THEFT",
+                "0890,THEFT,FROM BUILDING",
+                "1150,DECEPTIVE ,CREDIT CARD FRAUD",
+                "1310,CRIMINAL DAMAGE,TO PROPERTY",
+                "1320,CRIMINAL DAMAGE,TO VEHICLE",
+                "1365,CRIMINAL TRESPASS,TO RESIDENCE",
+                "1710,INVOLVING CHILDREN,ENDANGERING",
+                "2027,NARCOTICS,POSS: CRACK",
+                "2093,NARCOTICS,FOUND SUSPECT",
+                "4625,OTHER OFFENSE,PAROLE VIOLATION",
+                "5011,OTHER OFFENSE,LICENSE VIOLATION",
+                "031A,ROBBERY,ARMED: HANDGUN",
+                "041A,BATTERY,AGGRAVATED: HANDGUN",
+                "051A,ASSAULT,AGGRAVATED: HANDGUN",
+                "141A,WEAPONS VIOLATION,HANDGUN"
+           });
+            this.comboBox2.Location = new System.Drawing.Point(125, 89);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(260, 21);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59",
+            "60",
+            "61",
+            "62",
+            "63",
+            "64",
+            "65",
+            "66",
+            "67",
+            "68",
+            "69",
+            "70",
+            "71",
+            "72",
+            "73",
+            "74",
+            "75",
+            "76",
+            "77"});
+            this.comboBox1.Location = new System.Drawing.Point(293, 45);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(92, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 91);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Select Crime Type";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 50);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Select Area";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(80, 175);
+            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(91, 36);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Back";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(409, 297);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.welcomeLable1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "HMM Prediction Program";
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label welcomeLable1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ GuiHmm code/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace GuiHMM
+{
+    public partial class Form1 : Form
+    {
+        int area = -1;
+        string iusr = "";
+        int Resolution = -1;
+        double hmmProb = 0.0;
+        double normalStat = 0.0;
+        double impProb = 0.0;
+        double gangProb = 0.0;
+        string today = "";
+
+        List<Panel> listPanel = new List<Panel>();
+        [DllImport("HmmDll.dll", CharSet = CharSet.Unicode)]
+        public static extern int checkProgrameState();
+        [DllImport("HmmDll.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void getLastDate(StringBuilder myString, int length);
+        public Form1()
+        {
+            string date = "xx/xx/xx";
+            if (checkProgrameState() == 1)
+            {
+                StringBuilder str = new StringBuilder(100);
+                //get my string from native code
+                getLastDate(str, str.Capacity);
+                date = str.ToString();
+            }
+            InitializeComponent();
+            label1.Text = "*The data set until " + date;
+        }
+        //Continu Withot Upload
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (checkProgrameState() == 0)
+            {
+                MessageBox.Show("You Must Upload Data", "No Data Eror", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                panel1.Visible = true;
+            }
+        }
+        [DllImport("HmmDll.dll", CharSet = CharSet.Unicode)]
+        public static extern int startLoad();
+        //upload file
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Uploading Data may take up to a minute");
+            if (startLoad() == 1)
+            {
+                StringBuilder str = new StringBuilder(100);
+                //get my string from native code
+                getLastDate(str, str.Capacity);
+                label1.Text = "*The data set until " + str.ToString();
+                button2.Text = "Continue";
+                MessageBox.Show("Data is Set!");
+                panel1.Visible = true;
+            }
+        }
+        //get prediction
+        [DllImport("HmmDll.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void createSeq(int a, string b);
+        [DllImport("HmmDll.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern double getStatPrediction(int startingYear, int area, string iusr, int crimeResolution, int mode, string date);
+        [DllImport("HmmDll.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern double getPrediction(int a, string b ,int c);
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                Object selectedArea = comboBox1.SelectedItem;
+                area = Int32.Parse(selectedArea.ToString());
+            }
+            if (comboBox2.SelectedItem != null)
+            {
+                Object selectedIusr = comboBox2.SelectedItem;
+                iusr = selectedIusr.ToString();
+            }
+            if (comboBox3.SelectedItem != null)
+            {
+                Object selectedRes = comboBox3.SelectedItem;
+                Resolution = Int32.Parse(selectedRes.ToString());
+            }
+            if (area != -1 && iusr != "" && Resolution != -1)
+            {
+                createSeq(area, iusr);
+                hmmProb = getPrediction(area, iusr, Resolution);
+
+                StringBuilder str = new StringBuilder(100);
+                //get my string from native code
+                getLastDate(str, str.Capacity);
+                today = str.ToString();
+                normalStat = getStatPrediction(2003, area, iusr, Resolution, 0, today);
+                impProb = getStatPrediction(2003, area, iusr, Resolution, 1, today);
+                gangProb = getStatPrediction(2003, area, iusr, Resolution, 2, today);
+
+                normalStat = normalStat * 100;
+                impProb = impProb * 100;
+                gangProb = gangProb * 100;
+                panel2.Visible = true;
+                panel1.Visible = false;
+
+            }
+            else
+            {
+                MessageBox.Show("You must fil all required fields", "Input Eror", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+        //output - table[IUCR,COMM.AREA,TIME,DATE]
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            DataTable table = new DataTable();
+
+            table.Columns.Add("HMM", typeof(double));
+            table.Columns.Add("Statistical method", typeof(double));
+            table.Columns.Add("Improved Prediction", typeof(double));
+            table.Columns.Add("Gang Probability", typeof(double));
+            table.Rows.Add(hmmProb, normalStat, impProb, gangProb);
+
+            dataGridView1.DataSource = table;
+        }
+        //back to prediction parameters
+        private void button4_Click(object sender, EventArgs e)
+        {
+            panel2.Visible = false;
+            panel1.Visible = true;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listPanel.Add(panel1);
+            listPanel.Add(panel2);
+        }
+         
+        //Show State History
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Process.Start("fsrc\\stateHistory.txt");
+        }
+
+        //back from get prediction to main window
+        private void button6_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+        }
+    }
+}
